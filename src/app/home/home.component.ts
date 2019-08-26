19173a57fd73ae04ed585709f90af3d0d0ad99cf
Andrés Salgado <andressalgado41@gmail.com>
2019-08-26 12:46:49 -0500

    Plugin Flashlight

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from "@angular/core";
 import { RouterExtensions } from "nativescript-angular/router";
 import { Page } from "tns-core-modules/ui/page/page";
 
-import * as firebase from 'nativescript-plugin-firebase';
+import * as flashlight from "nativescript-flashlight";
+
 import { AuthFirebaseService } from "../services/auth-firebase.service";
+
 
 @Component({
     selector: "Home",
@@ -13,6 +15,9 @@ import { AuthFirebaseService } from "../services/auth-firebase.service";
 })
 export class HomeComponent implements OnInit {
 
+    flashlightState:string = 'TURN ON';
+    state: boolean = false;
+
     constructor(private page:Page, private router:RouterExtensions, public _auth:AuthFirebaseService) {
         // Use the component constructor to inject providers.
     }
@@ -21,9 +26,33 @@ export class HomeComponent implements OnInit {
         // Init your component properties here.
         // this.page.actionBarHidden=true;
     }
+    
+    encender(){
+        
+        if (flashlight.isAvailable()) {
 
-    logout(){
+            if (!this.state) {
+
+                flashlight.on();
+                this.flashlightState = 'TURN OFF';
+                this.state = true;
+                
+            } else {
+
+                flashlight.off();
+                this.flashlightState = 'TURN ON';
+                this.state = false;
+
+            }
+        } else {
+            alert("la linterna no es accesible en el dispositivo");
+        }
+
+    }
+    
+
+    logout() {
         this._auth.logout();
-      }
+    }
 
 }
